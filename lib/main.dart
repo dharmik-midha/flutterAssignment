45fa74804b943c8cef9a584fc7546b7829b6c698
Dharmik Midha <82962472+dharmik-midha@users.noreply.github.com>
2021-12-18 23:45:08 +0530

    flutter assignment

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:assignment/home_screen.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.white, // navigation bar color
+    statusBarColor: Colors.white, // status bar color
+    statusBarBrightness: Brightness.dark, //status bar brigtness
+    statusBarIconBrightness: Brightness.dark, //status barIcon Brightness
+    systemNavigationBarDividerColor:
+        Colors.white, //Navigation bar divider color
+    systemNavigationBarIconBrightness: Brightness.dark, //navigation bar icon
+  ));
+  runApp(
+      const MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
+}
